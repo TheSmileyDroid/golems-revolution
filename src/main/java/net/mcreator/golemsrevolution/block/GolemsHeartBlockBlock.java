56f767357126951e08ec77c59db3f9a1dd8fc761
TheSmileyDroid <ghsilha@gmail.com>
2021-08-02 22:28:10 -0300

    begin

---
+++ src/main/java/net/mcreator/golemsrevolution/block/GolemsHeartBlockBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.golemsrevolution.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.golemsrevolution.GolemsRevolutionModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@GolemsRevolutionModElements.ModElement.Tag
+public class GolemsHeartBlockBlock extends GolemsRevolutionModElements.ModElement {
+	@ObjectHolder("golems_revolution:golems_heart_block")
+	public static final Block block = null;
+	public GolemsHeartBlockBlock(GolemsRevolutionModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 10f).setLightLevel(s -> 0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("golems_heart_block");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
