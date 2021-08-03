dff966961b0e9e59a4638c2c8a0cfdd5ca3091d9
PoliCinetico <nicol@LAPTOP-24QK5QM3>
2021-08-02 23:11:21 -0300

    Alma do golem teste 1

---
+++ src/main/java/net/mcreator/golemsrevolution/item/GolemsSoulItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.golemsrevolution.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.golemsrevolution.GolemsRevolutionModElements;
+
+@GolemsRevolutionModElements.ModElement.Tag
+public class GolemsSoulItem extends GolemsRevolutionModElements.ModElement {
+	@ObjectHolder("golems_revolution:golems_soul")
+	public static final Item block = null;
+	public GolemsSoulItem(GolemsRevolutionModElements instance) {
+		super(instance, 7);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("golems_soul");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
