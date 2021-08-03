ea14aadeb1ad5706cbe98f7f3116ab8560be061b
TheSmileyDroid <ghsilha@gmail.com>
2021-08-03 10:47:18 -0300

    Inventario para golems

--- src/main/java/net/mcreator/golemsrevolution/entity/LittleGolemEntity.java
+++ src/main/java/net/mcreator/golemsrevolution/entity/LittleGolemEntity.java
@@ -2,41 +2,64 @@
 package net.mcreator.golemsrevolution.entity;
 
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.items.wrapper.EntityHandsInvWrapper;
+import net.minecraftforge.items.wrapper.EntityArmorInvWrapper;
+import net.minecraftforge.items.wrapper.CombinedInvWrapper;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.fml.network.FMLPlayMessages;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.DungeonHooks;
 
-import net.minecraft.world.gen.Heightmap;
-import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Direction;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.IPacket;
+import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.item.SpawnEggItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
-import net.minecraft.entity.monster.MonsterEntity;
-import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.ai.goal.RandomWalkingGoal;
-import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.entity.ai.goal.LookRandomlyGoal;
 import net.minecraft.entity.ai.goal.HurtByTargetGoal;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.CreatureAttribute;
+import net.minecraft.enchantment.EnchantmentHelper;
 
+import net.mcreator.golemsrevolution.gui.GolemInventoryGui;
 import net.mcreator.golemsrevolution.entity.renderer.LittleGolemRenderer;
 import net.mcreator.golemsrevolution.GolemsRevolutionModElements;
+
+import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
+
+import io.netty.buffer.Unpooled;
 
 @GolemsRevolutionModElements.ModElement.Tag
 public class LittleGolemEntity extends GolemsRevolutionModElements.ModElement {
@@ -47,25 +70,18 @@ public class LittleGolemEntity extends GolemsRevolutionModElements.ModElement {
 		super(instance, 8);
 		FMLJavaModLoadingContext.get().getModEventBus().register(new LittleGolemRenderer.ModelRegisterHandler());
 		FMLJavaModLoadingContext.get().getModEventBus().register(new EntityAttributesRegisterHandler());
-		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@Override
 	public void initElements() {
 		elements.entities.add(() -> entity);
-		elements.items.add(() -> new SpawnEggItem(entity, -16711681, -13421569, new Item.Properties().group(ItemGroup.MISC))
+		elements.items.add(() -> new SpawnEggItem(entity, -16751002, -16764058, new Item.Properties().group(ItemGroup.MISC))
 				.setRegistryName("little_golem_spawn_egg"));
-	}
-
-	@SubscribeEvent
-	public void addFeatureToBiomes(BiomeLoadingEvent event) {
-		event.getSpawns().getSpawner(EntityClassification.MONSTER).add(new MobSpawnInfo.Spawners(entity, 20, 4, 4));
 	}
 
 	@Override
 	public void init(FMLCommonSetupEvent event) {
-		EntitySpawnPlacementRegistry.register(entity, EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
-				MonsterEntity::canMonsterSpawn);
+		DungeonHooks.addDungeonMob(entity, 180);
 	}
 	private static class EntityAttributesRegisterHandler {
 		@SubscribeEvent
@@ -88,6 +104,7 @@ public class LittleGolemEntity extends GolemsRevolutionModElements.ModElement {
 			super(type, world);
 			experienceValue = 0;
 			setNoAI(false);
+			enablePersistence();
 		}
 
 		@Override
@@ -98,16 +115,19 @@ public class LittleGolemEntity extends GolemsRevolutionModElements.ModElement {
 		@Override
 		protected void registerGoals() {
 			super.registerGoals();
-			this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2, false));
-			this.goalSelector.addGoal(2, new RandomWalkingGoal(this, 1));
-			this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
-			this.goalSelector.addGoal(4, new LookRandomlyGoal(this));
-			this.goalSelector.addGoal(5, new SwimGoal(this));
+			this.goalSelector.addGoal(1, new RandomWalkingGoal(this, 1));
+			this.targetSelector.addGoal(4, new HurtByTargetGoal(this).setCallsForHelp(this.getClass()));
+			this.goalSelector.addGoal(5, new LookRandomlyGoal(this));
 		}
 
 		@Override
 		public CreatureAttribute getCreatureAttribute() {
 			return CreatureAttribute.UNDEFINED;
+		}
+
+		@Override
+		public boolean canDespawn(double distanceToClosestPlayer) {
+			return false;
 		}
 
 		@Override
@@ -118,6 +138,78 @@ public class LittleGolemEntity extends GolemsRevolutionModElements.ModElement {
 		@Override
 		public net.minecraft.util.SoundEvent getDeathSound() {
 			return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.generic.death"));
+		}
+		private final ItemStackHandler inventory = new ItemStackHandler(9) {
+			@Override
+			public int getSlotLimit(int slot) {
+				return 64;
+			}
+		};
+		private final CombinedInvWrapper combined = new CombinedInvWrapper(inventory, new EntityHandsInvWrapper(this),
+				new EntityArmorInvWrapper(this));
+		@Override
+		public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction side) {
+			if (this.isAlive() && capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY && side == null)
+				return LazyOptional.of(() -> combined).cast();
+			return super.getCapability(capability, side);
+		}
+
+		@Override
+		protected void dropInventory() {
+			super.dropInventory();
+			for (int i = 0; i < inventory.getSlots(); ++i) {
+				ItemStack itemstack = inventory.getStackInSlot(i);
+				if (!itemstack.isEmpty() && !EnchantmentHelper.hasVanishingCurse(itemstack)) {
+					this.entityDropItem(itemstack);
+				}
+			}
+		}
+
+		@Override
+		public void writeAdditional(CompoundNBT compound) {
+			super.writeAdditional(compound);
+			compound.put("InventoryCustom", inventory.serializeNBT());
+		}
+
+		@Override
+		public void readAdditional(CompoundNBT compound) {
+			super.readAdditional(compound);
+			INBT inventoryCustom = compound.get("InventoryCustom");
+			if (inventoryCustom instanceof CompoundNBT)
+				inventory.deserializeNBT((CompoundNBT) inventoryCustom);
+		}
+
+		@Override
+		public ActionResultType func_230254_b_(PlayerEntity sourceentity, Hand hand) {
+			ItemStack itemstack = sourceentity.getHeldItem(hand);
+			ActionResultType retval = ActionResultType.func_233537_a_(this.world.isRemote());
+			if (sourceentity instanceof ServerPlayerEntity) {
+				NetworkHooks.openGui((ServerPlayerEntity) sourceentity, new INamedContainerProvider() {
+					@Override
+					public ITextComponent getDisplayName() {
+						return new StringTextComponent("Little Golem");
+					}
+
+					@Override
+					public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
+						PacketBuffer packetBuffer = new PacketBuffer(Unpooled.buffer());
+						packetBuffer.writeBlockPos(new BlockPos(sourceentity.getPosition()));
+						packetBuffer.writeByte(0);
+						packetBuffer.writeVarInt(CustomEntity.this.getEntityId());
+						return new GolemInventoryGui.GuiContainerMod(id, inventory, packetBuffer);
+					}
+				}, buf -> {
+					buf.writeBlockPos(new BlockPos(sourceentity.getPosition()));
+					buf.writeByte(0);
+					buf.writeVarInt(this.getEntityId());
+				});
+			}
+			super.func_230254_b_(sourceentity, hand);
+			double x = this.getPosX();
+			double y = this.getPosY();
+			double z = this.getPosZ();
+			Entity entity = this;
+			return retval;
 		}
 	}
 }
